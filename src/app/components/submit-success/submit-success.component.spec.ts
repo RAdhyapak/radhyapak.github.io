d710e9270309d938f001f03dff72ad5ade6b9331
RAdhyapak <rohan.adhyapak@gmail.com>
2021-09-18 20:58:35 +0530

    angular update 8 to 12

--- src/app/components/submit-success/submit-success.component.spec.ts
+++ src/app/components/submit-success/submit-success.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SubmitSuccessComponent } from './submit-success.component';
 
@@ -6,7 +6,7 @@ describe('SubmitSuccessComponent', () => {
   let component: SubmitSuccessComponent;
   let fixture: ComponentFixture<SubmitSuccessComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SubmitSuccessComponent ]
     })
--- src/app/components/vertical-space/vertical-space.component.spec.ts
+++ src/app/components/vertical-space/vertical-space.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { VerticalSpaceComponent } from './vertical-space.component';
 
@@ -6,7 +6,7 @@ describe('VerticalSpaceComponent', () => {
   let component: VerticalSpaceComponent;
   let fixture: ComponentFixture<VerticalSpaceComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ VerticalSpaceComponent ]
     })
